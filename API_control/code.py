ffe89a8376da11c6a090ea1bd839c13450ce69c4
anotherAsad <ashaq.bee16seecs@seecs.edu.pk>
2024-12-07 02:14:55 -0500

    MultiEdgeFix, UpdateMetadata, BetterAsync, JSONdump

--- API_control/code.py
+++ API_control/code.py
@@ -16,31 +16,35 @@ app = FastAPI()
 pagerank_call_count = 0
 
 ######################################## UI/UX INTERFACE ########################################
-def get_graph_as_json():
-	json_graph = graph_manip.convert_graph_to_JSON(graph_manip.g)
-	return json_graph
-
 @app.get("/uiux/graph")
 async def return_graph():
-	graph_manip.update_mutex.acquire()		# acquire mutex lock
-	ret_val = get_graph_as_json()
-	graph_manip.update_mutex.release()		# release mutex lock
+	graph_manip.graph_update_mutex.acquire()		# acquire mutex lock
+	json_graph = graph_manip.convert_graph_to_JSON(graph_manip.g)
+	graph_manip.graph_update_mutex.release()		# release mutex lock
 	
-	return ret_val
+	return json_graph
 
 # TODO: implement sub graphing
-@app.get("/uiux/subgraph/{url}")
-async def return_subgraph(url):
-	graph_manip.update_mutex.acquire()		# acquire mutex lock
-	ret_val = get_graph_as_json()
-	graph_manip.update_mutex.release()		# release mutex lock
+@app.get("/uiux/subgraph/{url}/{depth}")
+async def return_subgraph(url:str, depth:int):
+	graph_manip.graph_update_mutex.acquire()		# acquire mutex lock
 	
-	return ret_val
+	# create a subgraph.
+	subg, urls, rank, click_count = graph_manip.get_subgraph(url, depth)
+
+	# return None if the URL was not found
+	if subg == None:
+		json_graph = {}
+	else:
+		json_graph = graph_manip.convert_subgraph_to_JSON(subg, urls, rank, click_count)
+
+	graph_manip.graph_update_mutex.release()		# release mutex lock	
+	return json_graph
 
 ####################################### RANKING INTERFACE #######################################
 @app.get("/ranking/{url}")
-async def return_score(url):
-	graph_manip.update_mutex.acquire()		# acquire mutex lock
+async def return_score(url:str):
+	graph_manip.graph_update_mutex.acquire()		# acquire mutex lock
 
 	node = graph_manip.find_in_graph(url)
 	return_json = {"pageRank": None, "inLinkCount": None, "outLinkCount": None}
@@ -51,7 +55,7 @@ async def return_score(url):
 		return_json["outLinkCount"] = int(graph_manip.g.get_out_degrees([node])[0])
 		print(return_json)
 
-	graph_manip.update_mutex.release()		# release mutex lock
+	graph_manip.graph_update_mutex.release()		# release mutex lock
 
 	return return_json
 ###################################### CRAWLING INTERFACE #######################################
@@ -61,18 +65,18 @@ class crawling_payload(BaseModel):
 
 @app.post("/crawling/add_nodes")
 async def update_link_graph(add_node_payload: crawling_payload):
-	graph_manip.update_mutex.acquire()		# acquire mutex lock
+	graph_manip.graph_update_mutex.acquire()		# acquire mutex lock
 	# call the wrapped function
-	graph_manip.add_node(add_node_payload.json())
-	graph_manip.update_mutex.release()		# release mutex lock
+	graph_manip.add_nodes(add_node_payload.json())
+	graph_manip.graph_update_mutex.release()		# release mutex lock
 
 	return {"nodes_added": True}
 
 @app.get("/crawling/remove_node/{url}")
-async def remove_node(url):
-	graph_manip.update_mutex.acquire()		# acquire mutex lock
+async def remove_node(url:str):
+	graph_manip.graph_update_mutex.acquire()		# acquire mutex lock
 	result = graph_manip.remove_node(url)
-	graph_manip.update_mutex.release()		# release mutex lock
+	graph_manip.graph_update_mutex.release()		# release mutex lock
 
 	return {"node_removed": result}
 
@@ -83,12 +87,12 @@ class evaluation_payload(BaseModel):
 	click_count: list
 
 @app.post("/evaluation/update_metadata")
-async def update_metadata(evaluation_payload):
-	graph_manip.update_mutex.acquire()		# acquire mutex lock
-	graph_manip.update_metadata(evaluation_payload.json())
-	graph_manip.update_mutex.release()		# release mutex lock
+async def update_metadata(add_eval_payload: evaluation_payload):
+	graph_manip.graph_update_mutex.acquire()		# acquire mutex lock
+	return_code = graph_manip.update_metadata(add_eval_payload)
+	graph_manip.graph_update_mutex.release()		# release mutex lock
 
-	return True
+	return return_code
 
 # Report metric is called by US.
 
@@ -100,41 +104,26 @@ async def run_pagerank():
 
 @app.get("/visualize_graph")
 async def visualize_graph():
-	graph_manip.update_mutex.acquire()		# acquire mutex lock
-
-	graph_manip.graph_tool.draw.graph_draw(
-		graph_manip.g, 
-		edge_pen_width=5,
-		vertex_text=graph_manip.node_text, 
-		vertex_aspect=1, 
-		vertex_text_position=1, 
-		vertex_text_color='black',
-		vertex_font_family='sans',
-		vertex_font_size=11,
-		vertex_color=None,
-		vertex_size=20,
-		output="new_graph.png"
-	)
-
-	graph_manip.update_mutex.release()		# release mutex lock
+	graph_manip.visualize_graph("new_graph.png")
 
 	return True
 
 @app.get("/print_scores")
 async def print_scores():
-	graph_manip.update_mutex.acquire()		# acquire mutex lock
+	graph_manip.graph_update_mutex.acquire()		# acquire mutex lock
 
 	if graph_manip.page_rank is not None:
 		print("\nPageRank scores:")
 		for v in graph_manip.g.vertices():
 			print(f"{graph_manip.node_url[v]}: {graph_manip.page_rank[v]}")
 
-	graph_manip.update_mutex.release()		# release mutex lock
+	graph_manip.graph_update_mutex.release()		# release mutex lock
 
 	return True
 
 @app.get("/print_stats")
-async def print_scores():
+async def print_stats():
+	global pagerank_call_count
 	print(f"pagerank_call_count: {pagerank_call_count}")
 
 	return True
@@ -145,96 +134,97 @@ async def print_scores():
 # Updates page rank
 def update_pagerank():
 	global pagerank_call_count
+
+	# Do not run page_rank if the graph is empty
+	if graph_manip.g.num_vertices() == 0:
+		return False
+
 	# acquire mutex
-	graph_manip.update_mutex.acquire()
+	graph_manip.graph_update_mutex.acquire()
 
 	pagerank_call_count += 1
 	print(f"[PageRank Call: {pagerank_call_count}]")
 
 	graph_manip.page_rank = graph_manip.pagerank(graph_manip.g)
 
-	# update the text that is to be shown in graph visualization
-	for node in graph_manip.g.vertices():
-		graph_manip.node_text[node] = f"{graph_manip.node_url[node]}: {graph_manip.page_rank[node]:.3f}"
-
 	# release mutex
-	graph_manip.update_mutex.release()
+	graph_manip.graph_update_mutex.release()
 
-	return
+	return True
 
-#################################################################################################
+################################################################################################################
 #testing and driver code. Will ~not~ be used in the final product.
-payload = '[{"url":"www.google.com","child_nodes":["www.abc.com","www.def.com"]},{"url":"www.rpi.edu","child_nodes":["www.google.com","www.abc.com"]}]'
 
-graph_manip.add_node(payload)
+if True:
+	payload = '[{"url":"www.google.com","child_nodes":["www.abc.com","www.def.com"]},{"url":"www.rpi.edu","child_nodes":["www.google.com","www.abc.com"]}]'
 
-graph_manip.add_node('{"url":"www.boogle.com"}')
+	graph_manip.add_nodes(payload)
 
-update_pagerank()
+	graph_manip.add_nodes('{"url":"www.boogle.com"}')
 
-graph_manip.graph_tool.draw.graph_draw(
-	graph_manip.g, 
-	edge_pen_width=5,
-	vertex_text=graph_manip.node_text, 
-	vertex_aspect=1, 
-	vertex_text_position=1, 
-	vertex_text_color='black',
-	vertex_font_family='sans',
-	vertex_font_size=11,
-	vertex_color=None,
-	vertex_size=20,
-	output="mygraph.png"
-)
+	update_pagerank()
 
-graph_manip.remove_node("www.abc.com")
+	graph_manip.visualize_graph("mygraph.png")
 
-update_pagerank()
+	graph_manip.remove_node("www.abc.com")
 
-graph_manip.graph_tool.draw.graph_draw(
-	graph_manip.g, 
-	edge_pen_width=5,
-	vertex_text=graph_manip.node_text, 
-	vertex_aspect=1, 
-	vertex_text_position=1, 
-	vertex_text_color='black',
-	vertex_font_family='sans',
-	vertex_font_size=11,
-	vertex_color=None,
-	vertex_size=20,
-	output="delgraph.png"
-)
+	update_pagerank()
 
-if graph_manip.page_rank is not None:
-	print("\nPageRank scores:")
-	for v in graph_manip.g.vertices():
-		print(f"{graph_manip.node_url[v]}: {graph_manip.page_rank[v]}")
+	graph_manip.visualize_graph("delgraph.png")
 
-
+	if graph_manip.page_rank is not None:
+		print("\nPageRank scores:")
+		for v in graph_manip.g.vertices():
+			print(f"{graph_manip.node_url[v]}: {graph_manip.page_rank[v]}")
 				
-print("Doing Good. Will start pagerank thread")
+print("Doing Good. Will spawn timed_processing_loop and run_server")
 
 
 ################################################################################################################
 # Main Routine
 shutdown = False
 
-async def pagerank_routine():
+# DESCRIPTION: Calls pagerand and Performs JSON dumps of graph after set interval.
+async def timed_processing_loop():
+	# BOOTUP SECTION
+	# TODO: Read from the stored json
+
+	get_graph_as_json = lambda: graph_manip.convert_graph_to_JSON(graph_manip.g)
+
+	# RUNNING SECTION
 	while not shutdown:
 		update_pagerank()
 		await asyncio.sleep(10)
 
+		# After every 15 minutes save the graph
+		if pagerank_call_count > 1 and pagerank_call_count % 10 == 0:
+			with open("graph.json", "w") as file:
+				json.dump(get_graph_as_json(), file, indent=4)
+
+	# SHUTDOWN SECTION: whenever the system shuts down, we save the graph downstream 
+	with open("graph.json", "w") as file:
+		json.dump(get_graph_as_json(), file, indent=4)
+
+	print("JSON dump of graph complete!\nClosing timed_processing_loop.")
+	return
+
+# DESCRIPTION: Spawns an HTTP server that is used for interfacing with components of other teams.
 async def run_server():
 	global shutdown
-	
+
 	config = uvicorn.Config("code:app", port=1234, log_level="info", host="lspt-link-analysis.cs.rpi.edu", reload=True)
 	server = uvicorn.Server(config)
 	await server.serve()
 
 	shutdown = True
 
+	print("Server shutdown complete.")
+	return
+
+# Main routine
 async def main():
     server_task = asyncio.create_task(run_server())
-    action_task = asyncio.create_task(pagerank_routine())
+    action_task = asyncio.create_task(timed_processing_loop())
     await asyncio.gather(server_task, action_task)
 
 if __name__ == "__main__":
--- API_control/graph_manip.py
+++ API_control/graph_manip.py
@@ -8,24 +8,23 @@ import json
 '''
 example payload:
 {
-    [
-        "url": "www.google.com",
-        "child_nodes": [
-          "abc.com",
-          "def.com"
-        ]
-    ]   
-
+    "url":"www.example.com",
+    "child_nodes":["www.abc.com", "www.def.com"]
 }
-
 '''
+
+# Mutex for graph update
 
 #create Graph as global so it is accessible everywhere
 g = Graph(directed=True)
+graph_update_mutex = threading.Lock()
+
+# graph properties
 node_url = g.new_vp("string") # attaches the URL to the vertices. This is useful for displaying the URL
 page_rank = None
 node_text = g.new_vp("string")
-update_mutex = threading.Lock()
+click_count = g.new_vp("int")
+
 
 # returns the vertex
 def find_in_graph(url):
@@ -56,7 +55,8 @@ def find_in_graph(url):
 
 '''
 
-def add_node(payload):
+# Adds a node and its children from the json. Doesn't perform redundant addition of edges and nodes.
+def add_nodes(payload):
     payload_list = json.loads(payload)
 
     # Handle both a single payload or a list of payloads
@@ -80,6 +80,7 @@ def add_node(payload):
             #add to graph
             parent_node = g.add_vertex()
             node_url[parent_node] = url
+            click_count[parent_node] = 0
 
         # case of stray node with no children
         if not "child_nodes" in item:
@@ -94,9 +95,11 @@ def add_node(payload):
             if child_node == None:
                 child_node = g.add_vertex()
                 node_url[child_node] = child
+                click_count[child_node] = 0
 
             # Create the required edge
-            g.add_edge(parent_node, child_node)
+            if not g.edge(parent_node, child_node):
+                g.add_edge(parent_node, child_node)
 
 	# success
     return 0
@@ -111,52 +114,117 @@ def remove_node(url):
 
     return False #failure
 
-# Is a stub function
+# Adds click count info to the graph
 def update_metadata(payload):
+    if len(payload.list_of_clicked_links) != len(payload.click_count):
+        print(f"ERR: graph_manip.update_metadata() expected equal list lengths")
+        return False
+
+    for idx in range(len(payload.list_of_clicked_links)):
+        node = find_in_graph(payload.list_of_clicked_links[idx])
+
+        if node != None:
+            click_count[node] += payload.click_count[idx]
+
+    return True
+
     # TODO: Write actual code
-    return False
+    return True
     
 # converts the graph to json
 def convert_graph_to_JSON(g):
     graph_data = {
-        "nodes": [{"id": int(v), "url": node_url[v], "page_rank": page_rank[v]} for v in g.vertices()],
+        "nodes": [{"id": int(v), "url": node_url[v], "page_rank": page_rank[v], "click_count": click_count[v]} for v in g.vertices()],
         "edges": [{"source": int(e.source()), "target": int(e.target())} for e in g.edges()]
     }
 
     return graph_data
+
+# converts the subgraph to json
+def convert_subgraph_to_JSON(subg, prop, pg, ck):
+    graph_data = {
+        "nodes": [{"id": int(v), "url": prop[v], "page_rank": pg[v], "click_count": ck[v]} for v in g.vertices()],
+        "edges": [{"source": int(e.source()), "target": int(e.target())} for e in g.edges()]
+    }
     
+    return graph_data
+    
+# 
+def get_subgraph(base_url, n):
+    visited = set()
+    
+    base = find_in_graph(base_url)
+
+    if base == None:
+        return None, None, None, None
+
+    queue = [(base, 0)]
+    
+    res = Graph()
+    urls = res.new_vp("string")
+    rank = res.new_vp("float")
+    clik = res.new_vp("string")
+    parent = None
+    
+    while queue:
+        v, d = queue.pop(0)
+        if v not in visited and d <= n:
+            visited.add(v)
+            #add new vector to subgraph and set property
+            new_v = res.add_vertex()
+            if parent:
+                res.add_edge(parent, new_v)
+            urls[new_v] = node_url[v]
+            rank[new_v] = page_rank[v]
+            clik[new_v] = click_count[v]
+
+            for child in g.get_out_neighbors(v):
+                queue.append((child, d + 1))
+                parent = new_v
+    
+    return res, urls, rank, clik
+
+
+# DESCRIPTION: prints the graph as a PNG
+def visualize_graph(filename):
+    assert isinstance(filename, str), f"ERR: graph_manip.add_node() expected `filename` as string"
+
+    if g.num_vertices() == 0:
+        return False
+
+    graph_update_mutex.acquire()		# acquire mutex lock
+
+    # update the text that is to be shown in graph visualization
+    for node in g.vertices():
+        node_text[node] = f"{node_url[node]}: {page_rank[node]:.3f}"
+
+    graph_tool.draw.graph_draw(
+        g,
+        bg_color=[1,1,1,1],
+        edge_pen_width=5,
+        vertex_text=node_text, 
+        vertex_aspect=1, 
+        vertex_text_position=1, 
+        vertex_text_color='black',
+        vertex_font_family='sans',
+        vertex_font_size=11,
+        vertex_color=None,
+        vertex_size=20,
+        output=filename
+    )
+
+    graph_update_mutex.release()		# release mutex lock
+
+    return True
+
 #testing and driver code. Will ~not~ be used in the final product.
 if __name__ == '__main__':
     payload = '[{"url":"www.google.com","child_nodes":["www.abc.com","www.def.com"]},{"url":"www.rpi.edu","child_nodes":["www.google.com","www.abc.com"]}]'
 
     add_node(payload)
 
-    graph_manip.graph_tool.draw.graph_draw(
-        graph_manip.g, 
-        edge_pen_width=5,
-        vertex_text=graph_manip.node_text, 
-        vertex_aspect=1, 
-        vertex_text_position=1, 
-        vertex_text_color='black',
-        vertex_font_family='sans',
-        vertex_font_size=11,
-        vertex_color=None,
-        vertex_size=20,
-        output="mygraph.png"
-    )
+    visualize_graph("mygraph.png")
 
     remove_node("www.abc.com")
 
-    graph_manip.graph_tool.draw.graph_draw(
-        graph_manip.g, 
-        edge_pen_width=5,
-        vertex_text=graph_manip.node_text, 
-        vertex_aspect=1, 
-        vertex_text_position=1, 
-        vertex_text_color='black',
-        vertex_font_family='sans',
-        vertex_font_size=11,
-        vertex_color=None,
-        vertex_size=20,
-        output="mygraph.png"
-    )
+    visualize_graph("delgraph.png")
